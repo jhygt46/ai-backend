a68561721e41255887dd3d4514d5e1c1316cdd44
jhygt46 <infolifeoperacional@gmail.com>
2020-08-10 18:26:21 -0400

    aa

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 io.on('connection', function(){ console.log("SERVER START"); });
-http.listen(80);
+
+http.listen(config.port, () => {
+    console.log("El servidor está inicializado en el puerto "+config.port);
+});
+
+http.get('/', function(req, res){
+
+    var correo = "buena@nelson.com";
+    io.emit('nuevo_correo', correo);
+
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Listo");
+    
+});
 
 /*
 
@@ -20,11 +33,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var config = JSON.parse(fs.readFileSync('./config.json'));
 
 
-app.listen(config.port, () => {
 
-    console.log("El servidor está inicializado en el puerto "+config.port);
-
-});
 
 app.get('/', urlencodedParser, function(req, res){
 
