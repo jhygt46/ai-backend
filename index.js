9cff8bc9f5f8b0b47b1e0a760876f5eee42f923a
jhygt46 <infolifeoperacional@gmail.com>
2020-08-26 23:08:01 -0400

    aa

--- index.js
+++ index.js
@@ -4,46 +4,20 @@ var io = require('socket.io')(http);
 
 io.on('connection', function(){ console.log("SERVER START"); });
 
+var fs = require('fs');
 var helpers = require('./helpers');
-http.listen(helpers.getPort(), () => {
-    console.log("El servidor está inicializado en el puerto "+helpers.getPort());
+var config = JSON.parse(fs.readFileSync('./config.json'));
+
+http.listen(config.port, () => {
+    fs.appendFile('init.log', 'Servidor iniciado a las ' + new Date().toLocaleString() + ' en puerto ' + config.port + '\n', function(err){ if(err) return console.log(err) });
 });
 
 app.get('/', function(req, res){
 
     var correo = "buena@nelson.com";
-    io.emit('cambiar_preciorffffffffffffffffv', correo);
+    io.emit('cambiar_precio', correo);
 
     res.setHeader('Content-Type', 'text/plain');
     res.end("Listo");
     
 });
-
-
-
-/*
-
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: true });
-var cors = require('cors');
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
-
-
-
-app.get('/', urlencodedParser, function(req, res){
-
-    var correo = "buena@nelson.com";
-    io.emit('nuevo_correo', correo);
-
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Listo");
-    
-});
-
-*/
